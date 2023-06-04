923f2b42a93466e4ec0a76d300884a7614040b40
ivashkovets <65442662+ivashkovets@users.noreply.github.com>
2023-06-04 18:34:26 +0300

    Add files via upload

---
+++ js/scrypt.js
@@ -0,0 +1,59 @@
+document.cockie='SameSite=Lax';
+var windowHeight = window.screen.height - 64;
+var width = (window.screen.width - 50) / 2;
+width = width + 'px';
+windowHeight = windowHeight + 'px';
+console.log('windowHeight', windowHeight);
+
+document.querySelector('#button-arrow').style.left = width;
+
+var pergament = document.querySelector('#pergament');
+pergament.style.height = windowHeight;
+pergament.style.overflow = 'hidden';
+pergament.style.overflowY = 'scroll';
+
+var header = document.querySelector('#header-content');
+header.style.height = windowHeight;
+header.style.display = 'flex';
+document.querySelector('#ramka').style.margin = 'auto 0';
+
+const deadline = new Date(2023, 7, 12);
+let timerId = null;
+console.log(deadline);
+console.log(new Date());
+
+function declensionNum(num, words) {
+    return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+}
+// вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+function countdownTimer() {
+    const diff = deadline - new Date();
+    if (diff <= 0) {
+        clearInterval(timerId);
+    }
+    const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+    const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+    const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+    $days.textContent = days < 10 ? '0' + days : days;
+    $hours.textContent = hours < 10 ? '0' + hours : hours;
+    $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+    //$seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+    $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+    $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+    $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+    //$seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+}
+
+const $days = document.querySelector('.timer__days');
+const $hours = document.querySelector('.timer__hours');
+const $minutes = document.querySelector('.timer__minutes');
+//const $seconds = document.querySelector('.timer__seconds');
+// вызываем функцию countdownTimer
+countdownTimer();
+// вызываем функцию countdownTimer каждую секунду
+timerId = setInterval(countdownTimer, 1000);
+
+pergament.addEventListener('scroll', function() {
+    document.querySelector('#button-arrow').style.display = 'none'
+});
